34bff73c23f1a76b416fe4c64d514a2bd5e130a6
Daniel Bailey <108521107+DanDWB@users.noreply.github.com>
2024-03-28 21:57:00 +0000

    Initial Psudocode

--- game1.py
+++ game1.py
@@ -1,3 +1,5 @@
+# PLANNING
+
 # To start this project I need to have a game idea/function planned: rock, paper, scissors
 
 # From here, I'm going to makesure I have the programming language that I will be using installed
@@ -5,3 +7,20 @@
 # here I used w3schools.com 
 
 # With these in mind, I can start the project
+
+# PSEDUCODE
+
+# Here I come I write the basic logic in psedo code in order to break the project down step by step,
+# but to also ensure it's in a readable format, so If I need to debug the logic far easier 
+# than If i tried to debug once I wrote everything in Python
+
+
+# 1) Take a users input, and store this as x, values can only be rock, paper, or scissors
+# 2) computer chooses random string between three options (Scissors, Rock, Paper) and stores as y
+# 3) Now I need a loop, to check results, so:
+# If x == y then print "you draw, you and the computer chose x"
+# else x 
+
+# or
+
+# 3) If     - so this is where I need to collect my thoughts for logic 

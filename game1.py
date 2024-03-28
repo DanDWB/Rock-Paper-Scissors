22e3c402063e8fc3ef1997241501783ea2e336ce
Daniel Bailey <108521107+DanDWB@users.noreply.github.com>
2024-03-28 21:48:03 +0000

    How I planned the project
    Here is how I planned the project, coming up with the function/game/point of the project, and following this I decided on the language. Here this will be Python due to the small entry barrier.

---
+++ game1.py
@@ -0,0 +1,7 @@
+# To start this project I need to have a game idea/function planned: rock, paper, scissors
+
+# From here, I'm going to makesure I have the programming language that I will be using installed
+# on my system, with a reliable source for the syntax, such as variables, strings, loops, etc
+# here I used w3schools.com 
+
+# With these in mind, I can start the project

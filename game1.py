5bf693d882555476d3627a8994933057c551ae40
Daniel Bailey <108521107+DanDWB@users.noreply.github.com>
2024-03-28 23:05:33 +0000

    Finishing my main logic and solving initial syntax errors
    Finishing main logic and solving initial issues with ==, and also the syntax errors and the :

--- game1.py
+++ game1.py
@@ -4,12 +4,12 @@ import random # had to import this to get the random choice selector
 # To start this project I need to have a game idea/function planned: rock, paper, scissors
 
 # From here, I'm going to makesure I have the programming language that I will be using installed
-# on my system, with a reliable source for the syntax, such as variables, strings, loops, etc
+# on my system, then find a reliable source for the syntax, such as variables, strings, loops, etc
 # here I used w3schools.com 
 
 # With these in mind, I can start the project
 
-# PSEDUCODE
+# PSEUDUCODE
 
 # Here I come I write the basic logic in psedo code in order to break the project down step by step,
 # but to also ensure it's in a readable format, so If I need to debug the logic far easier 
@@ -43,7 +43,7 @@ x = input("Which one do you choose? Rock, Paper, or Scissors? ") # for this, I l
 
 
 # I struggled to find this just by scrolling through w3 school, so typed this into google:
-# how to get the computer to choose a random string in python, coincidence the first choice
+# "how to get the computer to choose a random string in python", coincidence the first choice
 # was a w3 schools article showing the python random choice method
 
 complist = ["Rock", "Paper", "Scissors"] 
@@ -53,11 +53,16 @@ y = random.choice(complist)
 # before continuing, I tested the output to makesure this worked, breaking up the program further
 # It's way easier to debug in steps.
 
-if x = "Rock" and y = "Scissors" print ("You Win! Computer chose " + y)
-  if x = "Paper" and y = "Rock" print ("You Win! Computer chose " + y)
-    if x = "Scissors" and y = "Paper" print ("You Win! Computer chose " + y)
-    elif x == y print ("You both Draw! Computer chose " + y)
-    else print ("You Lose! Computer chose " + y)       # this is my first attempt, I will keep this 
+if x == "Rock" and y == "Scissors": 
+    print ("You Win! Computer chose " + y)
+if x == "Paper" and y == "Rock": 
+    print ("You Win! Computer chose " + y)
+    if x == "Scissors" and y == "Paper": 
+        print ("You Win! Computer chose " + y)
+    elif x == y: 
+        print ("You both Draw! Computer chose " + y)
+    else: 
+        print ("You Lose! Computer chose " + y)       # this is my first attempt, I will keep this 
 # here for visibility even if this is completely wrong. to get to this point
 # I scrolled to the if else portion of w3 schools and noticed 
 

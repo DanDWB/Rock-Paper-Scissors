1c7436a50244cc1e085243f253518afc78a5bf65
Daniel Bailey <108521107+DanDWB@users.noreply.github.com>
2024-03-29 17:11:50 +0000

    Fixing bug with Scissors whilst test
    Whilst testing, everything worked, except when you chose scissors, you could never win, as when the computer selected paper, it said "you lose", this was due to indentation of the if and elif statements

--- game1.py
+++ game1.py
@@ -57,8 +57,8 @@ if x.casefold() == "rock" and y == "Scissors":
     print ("You Win! Computer chose " + y)
     if x.casefold() == "paper" and y == "Rock":
        print ("You Win! Computer chose " + y)
-    if x.casefold() == "scissors" and y == "Paper": 
-          print ("You Win! Computer chose " + y)
+elif x.casefold() == "scissors" and y == "Paper": # for some reason the game was working until I selected scissors and y = paper, it said I lost, so this line needs work (SOLVED: instead of if, i did another elif, and indented it as shown, python is indentation sensitive)
+        print ("You Win! Computer chose " + y)
 elif x.casefold() == y.casefold(): # Here I fixed the issues with capital letters by typing on google: "how to remove capital sensitive in python strings", leading me to the .casefold() function on stack overflow
         print ("You both Draw! Computer chose " + y)
 else: 
